14a26e800b5cfd53d33f4c14bab66a1c48a87ec4
Pruebas Leoxt <129362802+PruebasMarth@users.noreply.github.com>
2023-05-25 12:30:08 -0600

    feis.php

--- feis.php
+++ feis.php
@@ -1,9 +1,9 @@
 <?php
   // The global $_POST variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
-  $actual = htmlspecialchars($_POST['actual']);
-  $nueva = htmlspecialchars($_POST['nueva']);
-  $nuevax2 = htmlspecialchars($_POST['nuevax2']);
+  $actual = htmlspecialchars ($_POST ['actual']);
+  $nueva = htmlspecialchars ($_POST ['nueva']);
+  $nuevax2 = htmlspecialchars ($_POST ['nuevax2']);
 
   echo $actual, '', $nueva, '', $nuevax2, '';
 ?>
