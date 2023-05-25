736297b6fcd8274029a710a3f95c0805f5edd0e3
Pruebas Leoxt <129362802+PruebasMarth@users.noreply.github.com>
2023-05-25 09:25:13 -0600

    feis.php

--- feis.php
+++ feis.php
@@ -1,9 +1,9 @@
 <?php
   // La variable global $ _POST que permite acceder a los datos enviados con el método POST por su nombre
   // Para acceder a los datos enviados con el método GET, puede usar $ _GET
-  $ = Actual htmlspecialchars ($ _POST [ 'actual']);
-  $ Nueva = htmlspecialchars ($ _POST [ 'nueva']);
-  $ Nuevax2 = htmlspecialchars ($ _POST [ 'nuevax2']);
+  $Actual = htmlspecialchars ($ _POST [ 'actual']);
+  $Nueva = htmlspecialchars ($ _POST [ 'nueva']);
+  $Nuevax2 = htmlspecialchars ($ _POST [ 'nuevax2']);
 
-  echo $ actual, '', $ nueva;
+  echo $actual, '', $nueva;
 ?>
